bc608bab7287458c406ea312ce7dec7e6ca520e8
tmanas06 <2210030003@klh.edu.in>
2026-03-13 23:26:39 +0530

    fix: logo asset and size refresh

--- src/app/home-page/page.js
+++ src/app/home-page/page.js
@@ -57,7 +57,7 @@ export default function HomePage() {
                 <div className="hero-content-split">
                     <div className="hero-left">
                         <img
-                            src="/logo.png"
+                            src="/logo_cache_bust_v1.png"
                             alt="We Heal The Earth Logo"
                             className="hero-large-logo"
                         />
